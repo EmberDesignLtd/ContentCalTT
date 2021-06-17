41edf56aab3cb96a643267a3800530d4fd740bc0
Sam M <emberdesignltd@protonmail.com>
2021-06-17 17:15:14 +0100

    feat(Theme): Implement quick theme button for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Renderer2 } from '@angular/core';
 
+enum Theme {
+  DARK = 'dark-theme',
+  LIGHT = 'light-theme',
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  private darkTheme = true;
+
+  constructor(private readonly renderer: Renderer2) {}
+
+  // TODO(Munro): Move to relevant component/section
+  toggleTheme() {
+    this.darkTheme = !this.darkTheme;
+    this.renderer.removeClass(
+      document.body,
+      this.darkTheme ? Theme.LIGHT : Theme.DARK
+    );
+    this.renderer.addClass(
+      document.body,
+      this.darkTheme ? Theme.DARK : Theme.LIGHT
+    );
+  }
+}
