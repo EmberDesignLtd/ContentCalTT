5df7e527c6d2c93344b550ffa197ca9f35fdca8c
Sam M <emberdesignltd@protonmail.com>
2021-06-17 17:49:13 +0100

    feat(IconModule): Add module and svg icon component

---
+++ src/app/svg-icon/svg-icon/svg-icon.component.ts
@@ -0,0 +1,34 @@
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+} from '@angular/core';
+
+export enum IconSize {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large',
+}
+
+export enum Icon {
+  THEME_SWITCH = 'theme-switch',
+}
+
+@Component({
+  selector: 'app-svg-icon',
+  templateUrl: './svg-icon.component.html',
+  styleUrls: ['./svg-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SvgIconComponent implements AfterViewInit {
+  @Input() iconId?: Icon;
+  @Input() iconSize = IconSize.MEDIUM;
+  @Input() flipped = false;
+
+  ngAfterViewInit() {
+    if (!this.iconId) {
+      console.warn('No iconId has been passed as an @Input!');
+    }
+  }
+}
