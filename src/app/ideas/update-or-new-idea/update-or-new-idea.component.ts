173cb7e0d9062110b6b7940acec8625600708acb
Sam M <emberdesignltd@protonmail.com>
2021-06-18 13:01:08 +0100

    feat(IdeaEntry): Add tag form controls

--- src/app/ideas/update-or-new-idea/update-or-new-idea.component.ts
+++ src/app/ideas/update-or-new-idea/update-or-new-idea.component.ts
@@ -1,7 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { IdeasService } from './../../services/ideas.service';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Idea, IdeasService } from './../../services/ideas.service';
 
+enum TagLabel {
+  AWESOME = 'Awesome',
+  PROGRESS = 'In Progess',
+  APPROVED = 'Approved',
+}
 @Component({
   selector: 'app-update-or-new-idea',
   templateUrl: './update-or-new-idea.component.html',
@@ -9,11 +14,16 @@ import { IdeasService } from './../../services/ideas.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UpdateOrNewIdeaComponent {
+  @Input() idea?: Idea;
   ideaForm = new FormGroup({
     title: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
     date: new FormControl(),
-    tags: new FormControl([]),
+    tags: new FormArray([
+      new FormControl({ label: TagLabel.AWESOME, value: false }),
+      new FormControl({ label: TagLabel.PROGRESS, value: true }),
+      new FormControl({ label: TagLabel.APPROVED, value: false }),
+    ]),
   });
 
   constructor(private readonly ideasService: IdeasService) {}
@@ -21,5 +31,11 @@ export class UpdateOrNewIdeaComponent {
   submit(): void {
     this.ideaForm.controls.date.setValue(new Date());
     this.ideasService.addNewEntry(this.ideaForm.value);
+  }
+
+  get tags(): FormControl[] {
+    // TODO(Munro): Work out type issue;
+    //@ts-ignore
+    return this.ideaForm.controls.tags.controls;
   }
 }
