cc89db6b2c93996d3ed300d39f20b427554a1319
Sam M <emberdesignltd@protonmail.com>
2021-06-18 19:56:32 +0100

    feat(Login): Change to readonly

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class LoginService {
   // Given more time I'd implement a more robust login service with expiration tokens/OAuth as a third party example
-  loggedIn = new BehaviorSubject(false);
+  readonly loggedIn = new BehaviorSubject(false);
 }
