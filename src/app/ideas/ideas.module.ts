56ceb74efe74161c3e9bff6a7ac7ce55f4256b1a
Sam M <emberdesignltd@protonmail.com>
2021-06-18 13:50:46 +0100

    feat(Ideas): Add card, button and title module

--- src/app/ideas/ideas.module.ts
+++ src/app/ideas/ideas.module.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from './../button/button.module';
+import { CardModule } from './../card/card.module';
+import { TitleModule } from './../title/title.module';
 import { IdeasRoutingModule } from './ideas-routing.module';
 import { IdeasComponent } from './ideas/ideas.component';
 import { UpdateOrNewIdeaComponent } from './update-or-new-idea/update-or-new-idea.component';
 
 @NgModule({
   declarations: [IdeasComponent, UpdateOrNewIdeaComponent],
-  imports: [CommonModule, IdeasRoutingModule, ReactiveFormsModule],
+  imports: [
+    ButtonModule,
+    CardModule,
+    CommonModule,
+    IdeasRoutingModule,
+    ReactiveFormsModule,
+    TitleModule,
+  ],
 })
 export class IdeasModule {}
