6b82b8e8f79d2dfbc0e6958fc67d51bdfe1aa6ba
Sam M <emberdesignltd@protonmail.com>
2021-06-18 16:47:17 +0100

    feat(Card): Extend card to act as a button

--- src/app/card/card/card.component.ts
+++ src/app/card/card/card.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
+  selector: 'app-card, button[app-card]',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class CardComponent {}
