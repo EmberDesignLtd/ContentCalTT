337ddec8cc2a5d22837af1952108a68540cb4da8
Sam M <emberdesignltd@protonmail.com>
2021-06-18 16:53:28 +0100

    feat(Ideas): Add svg and tag module

--- src/app/ideas/ideas.module.ts
+++ src/app/ideas/ideas.module.ts
@@ -3,19 +3,24 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from './../button/button.module';
 import { CardModule } from './../card/card.module';
+import { SvgIconModule } from './../svg-icon/svg-icon.module';
+import { TagModule } from './../tag/tag.module';
 import { TitleModule } from './../title/title.module';
 import { IdeasRoutingModule } from './ideas-routing.module';
 import { IdeasComponent } from './ideas/ideas.component';
+import { SortModalComponent } from './sort-modal/sort-modal.component';
 import { UpdateOrNewIdeaComponent } from './update-or-new-idea/update-or-new-idea.component';
 
 @NgModule({
-  declarations: [IdeasComponent, UpdateOrNewIdeaComponent],
+  declarations: [IdeasComponent, UpdateOrNewIdeaComponent, SortModalComponent],
   imports: [
     ButtonModule,
     CardModule,
     CommonModule,
     IdeasRoutingModule,
     ReactiveFormsModule,
+    SvgIconModule,
+    TagModule,
     TitleModule,
   ],
 })
