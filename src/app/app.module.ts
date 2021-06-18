c0c66e7f41eefda74b9152218e1c21780d383b2d
Sam M <emberdesignltd@protonmail.com>
2021-06-18 10:47:40 +0100

    feat(AppRouting): Add login, ideas and redirect routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './guards/auth-guard';
 import { HeaderModule } from './header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HeaderModule],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
