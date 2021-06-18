35f2a24e3ca4e9dd4df9940906fc53e1c56f51fe
Sam M <emberdesignltd@protonmail.com>
2021-06-18 10:46:19 +0100

    feat(IdeasModule): Add barebones ideas module and component

---
+++ src/app/ideas/ideas-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { IdeasComponent } from './ideas/ideas.component';
+
+const routes: Routes = [{ path: '', component: IdeasComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class IdeasRoutingModule {}
