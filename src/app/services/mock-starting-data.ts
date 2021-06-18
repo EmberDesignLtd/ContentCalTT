d58e45e2a7777c259977451abd0c165ed95f0bcc
Sam M <emberdesignltd@protonmail.com>
2021-06-18 16:51:01 +0100

    feat(IdeaData): Update mock data

--- src/app/services/mock-starting-data.ts
+++ src/app/services/mock-starting-data.ts
@@ -4,25 +4,41 @@ export const MOCK_DATA: Idea[] = [
   {
     title: 'Idea one',
     description: 'lorem ipsum',
-    tags: [{ label: 'Awesome', value: true }],
+    tags: [
+      { label: 'Awesome', value: true },
+      { label: 'In Progress', value: false },
+      { label: 'Approved', value: false },
+    ],
     date: new Date(),
   },
   {
     title: 'Idea two',
     description: 'lorem ipsum',
-    tags: [{ label: 'In progess', value: true }],
+    tags: [
+      { label: 'Awesome', value: true },
+      { label: 'In Progress', value: true },
+      { label: 'Approved', value: false },
+    ],
     date: new Date(),
   },
   {
     title: 'Idea three',
     description: 'lorem ipsum',
-    tags: [{ label: 'Approved', value: true }],
+    tags: [
+      { label: 'Awesome', value: true },
+      { label: 'In Progress', value: false },
+      { label: 'Approved', value: true },
+    ],
     date: new Date(),
   },
   {
     title: 'Idea four',
     description: 'lorem ipsum',
-    tags: [],
+    tags: [
+      { label: 'Awesome', value: false },
+      { label: 'In Progress', value: false },
+      { label: 'Approved', value: false },
+    ],
     date: new Date(),
   },
 ];
