128df994d855ac50349815a6b93870a252752907
Sam M <emberdesignltd@protonmail.com>
2021-06-18 10:43:54 +0100

    feat(LoginService): Add barebones login service

---
+++ src/app/login/login.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  // TODO(Munro): Implement a more realistic login behaviour with a expiration date & local storage
+  loggedIn = new BehaviorSubject(false);
+}
