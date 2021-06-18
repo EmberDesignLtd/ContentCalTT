9e3be9ec345a5336a85699ac32fdec86e056d89b
Sam M <emberdesignltd@protonmail.com>
2021-06-18 13:50:05 +0100

    feat(Login): Update styling

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from './../button/button.module';
+import { CardModule } from './../card/card.module';
+import { TitleModule } from './../title/title.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    ButtonModule,
+    CardModule,
+    CommonModule,
+    FormsModule,
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    TitleModule,
+  ],
 })
 export class LoginModule {}
