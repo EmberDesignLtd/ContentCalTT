a6371d1d8276393cb635acb6857e9ced83007a65
Sam M <emberdesignltd@protonmail.com>
2021-06-18 19:58:40 +0100

    feat(Title): Update change detection strat

--- src/app/title/title/title.component.ts
+++ src/app/title/title/title.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TitleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TitleComponent {}
